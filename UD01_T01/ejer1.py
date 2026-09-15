96f8b166842f84020d7247ffa5dad5b9f48a4c67
Iriaegea <iria.egea.fernandez@iessanmamede.com>
2026-09-15 12:12:12 +0200

    ejercicio1

---
+++ UD01_T01/ejer1.py
@@ -0,0 +1,6 @@
+# Conversor de temperaturas
+# Escribe un programa que pida al usuario una temperatura en grados Celsius y la convierta a Fahrenheit y Kelvin.
+
+celsius = int(input("Escribe una temperatura en Celsius para pasarla a Fahrenheit y Kelvin"))
+
+print(f"Celsius = {celsius}, Fahrenheit = {float((celsius*1,8)+32)}")
